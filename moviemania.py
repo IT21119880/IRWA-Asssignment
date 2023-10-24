da5e675712529f1a274eba2acb9336c5289c9fba
Nivasheni Balenthiren <87559443+IT21119880@users.noreply.github.com>
2023-10-24 10:45:28 +0530

    Update moviemania.py

--- moviemania.py
+++ moviemania.py
@@ -1,41 +1,59 @@
+import pickle
 import streamlit as st
-import pandas as pd
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.metrics.pairwise import cosine_similarity
+import requests
 
-# Read the data from a .csv file
-@st.cache(allow_output_mutation=True)
-def load_data(movies.csv):
-    data = pd.read_csv(movies.csv)
-    return data
+def fetch_poster(movie_id):
+    url = "https://api.themoviedb.org/3/movie/{}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US".format(movie_id)
+    data = requests.get(url)
+    data = data.json()
+    poster_path = data['poster_path']
+    full_path = "https://image.tmdb.org/t/p/w500/" + poster_path
+    return full_path
 
-def recommend_movies(movie_title, cosine_sim=cosine_sim):
-    # Get the index of the movie that matches the title
-    idx = movies_df[movies_df['title'] == movie_title].index[0]
-    
-    # Get the pairwsie similarity scores of all movies with that movie
-    sim_scores = list(enumerate(cosine_sim[idx]))
-    
-    # Sort the movies based on the similarity scores
-    sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
-    
-    # Get the scores of the 10 most similar movies
-    sim_scores = sim_scores[1:11]
-    
-    # Get the movie indices
-    movie_indices = [i[0] for i in sim_scores]
-    
-    # Return the top 10 most similar movies
-    return movies_df['title'].iloc[movie_indices]
+def recommend(movie):
+    index = movies[movies['title'] == movie].index[0]
+    distances = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda x: x[1])
+    recommended_movie_names = []
+    recommended_movie_posters = []
+    for i in distances[1:6]:
+        # fetch the movie poster
+        movie_id = movies.iloc[i[0]].movie_id
+        recommended_movie_posters.append(fetch_poster(movie_id))
+        recommended_movie_names.append(movies.iloc[i[0]].title)
 
-# Load the data from a .csv file
-movies_df = load_data('movies.csv')
+    return recommended_movie_names,recommended_movie_posters
 
-# Set up the app layout
-st.title('Movie Recommendation System')
 
-movie_title = st.text_input('Enter a movie title', 'Toy Story (1995)')
+st.header('Movie Recommender System')
+movies = pickle.load(open('model/movie_list.pkl','rb'))
+similarity = pickle.load(open('model/similarity.pkl','rb'))
 
-if st.button('Recommend'):
-    similar_movies = recommend_movies(movie_title)
-    st.write('Recommended Movies:'+ ', '.join(similar_movies))
+movie_list = movies['title'].values
+selected_movie = st.selectbox(
+    "Type or select a movie from the dropdown",
+    movie_list
+)
+
+if st.button('Show Recommendation'):
+    recommended_movie_names,recommended_movie_posters = recommend(selected_movie)
+    col1, col2, col3, col4, col5 = st.beta_columns(5)
+    with col1:
+        st.text(recommended_movie_names[0])
+        st.image(recommended_movie_posters[0])
+    with col2:
+        st.text(recommended_movie_names[1])
+        st.image(recommended_movie_posters[1])
+
+    with col3:
+        st.text(recommended_movie_names[2])
+        st.image(recommended_movie_posters[2])
+    with col4:
+        st.text(recommended_movie_names[3])
+        st.image(recommended_movie_posters[3])
+    with col5:
+        st.text(recommended_movie_names[4])
+        st.image(recommended_movie_posters[4])
+
+
+
+
